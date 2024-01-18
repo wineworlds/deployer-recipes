dd2cd3c6e48c83a47e7942f76744a6873c6334d6
Kubilay Melnikov <peksen.kubilay@gmail.com>
2024-01-18 21:39:45 +0100

    feat(v1.2.0): update shopware deployment

--- recipe/shopware-rsync.php
+++ recipe/shopware-rsync.php
@@ -2,6 +2,8 @@
 
 namespace Deployer;
 
+use Symfony\Component\Yaml\Yaml;
+
 require 'recipe/shopware.php';
 require 'contrib/rsync.php';
 require __DIR__ . '/fetch.php';
@@ -103,7 +105,7 @@ task('deploy', [
     'deploy:setup',
     'deploy:lock',
     'deploy:release',
-    'sw-build-without-db:build',
+    'sw-build-without-db',
     'rsync',
     'deploy:shared',
     'deploy:writable',
@@ -117,8 +119,15 @@ task('deploy', [
     'deploy:success',
 ]);
 
+task('sw-build-without-db', [
+    'sw-build-without-db:set-theme-config',
+    'sw-build-without-db:get-remote-config',
+    'sw-build-without-db:build',
+    'sw-build-without-db:remove-theme-config',
+]);
+
 task('sw-build-without-db:build', static function () {
-    runLocally('CI=1 SHOPWARE_SKIP_BUNDLE_DUMP=1 SHOPWARE_SKIP_THEME_COMPILE=1 PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1 ./bin/build-js.sh');
+    runLocally('CI=1 SHOPWARE_SKIP_NPM=1 SHOPWARE_SKIP_BUNDLE_DUMP=1 SHOPWARE_SKIP_THEME_COMPILE=1 PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1 ./bin/build-js.sh');
 });
 
 task('sw:deploy', [
@@ -129,3 +138,40 @@ task('sw:deploy', [
     'sw:cache:clear',
     'sw:database:migrate',
 ]);
+
+task('sw-build-without-db:set-theme-config', static function () {
+    $data = [
+        'storefront' => [
+            'theme' => [
+                'config_loader_id' => 'Shopware\Storefront\Theme\ConfigLoader\StaticFileConfigLoader',
+                'available_theme_provider' => 'Shopware\Storefront\Theme\ConfigLoader\StaticFileAvailableThemeProvider'
+            ]
+        ]
+    ];
+
+    $content = Yaml::dump($data, 3, 4);
+    $escapedContent = escapeshellarg($content);
+    $filePath = './config/packages/storefront.yaml';
+
+    runLocally("echo $escapedContent > $filePath");
+});
+
+task('sw-build-without-db:remove-theme-config', static function () {
+    $filePath = './config/packages/storefront.yaml';
+
+    runLocally("if [ -f $filePath ]; then rm $filePath; fi");
+});
+
+task('sw-build-without-db:get-remote-config', static function () {
+    if (!test('[ -d {{current_path}} ]')) {
+        return;
+    }
+    within('{{deploy_path}}/current', function () {
+        run('{{bin/php}} ./bin/console bundle:dump');
+        download('{{deploy_path}}/current/var/plugins.json', './var/');
+
+        run('{{bin/php}} ./bin/console theme:dump');
+        download('{{deploy_path}}/current/files/theme-config', './files/');
+        download('{{deploy_path}}/current/custom/plugins/', './custom/plugins');
+    });
+});
